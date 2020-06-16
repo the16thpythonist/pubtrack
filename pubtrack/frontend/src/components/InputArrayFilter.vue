acddc1397e00e2933fc13a3539234abb52d4db42
the16thpythonist <jonseb1998@gmail.com>
2020-06-16 16:54:17 +0200

    Documentation InputArrayFilter.vue

--- pubtrack/frontend/src/components/InputArrayFilter.vue
+++ pubtrack/frontend/src/components/InputArrayFilter.vue
@@ -15,22 +15,76 @@
 </template>
 
 <script>
+    // TODO: Add a checkbox to the front, so the filter can also be toggled.
+    /**
+     * A free text filter to be dynamically applied to an array
+     *
+     * First and foremost, the basic functionality of this component is the functionality of a filter for an array.
+     * An array gets passed in and based on some defined rules some elements of this array will be removed for the
+     * resulting array, which is returned.
+     * The array on which to be operated gets passed in as the "base" prop and the resulting array will be updated
+     * in the "value" prop.
+     *
+     * The component will render as a text input field. Whatever text will be entered into the input field will also
+     * be passed to the filter function.
+     * How the filter works specifically is not implemented within this component. This filter functionality can also
+     * be passed dynamically to the component using the "filter" prop.
+     * The filter defined there will be reapplied to the array every time the content of the input field changes
+     *
+     * EXAMPLE
+     *
+     * ```js
+     * export default {
+     *     name: "SomeComponent",
+     *     data: {
+     *         baseArray: ["Hello", "World"],
+     *         resultArray: [],
+     *         inputFilter: {
+     *             name:        "Contains Text",
+     *             placeholder: "Enter text to check for inclusion",
+     *             func(array, input) {
+     *                 return array.filter(function(element){
+     *                     return element.includes(input);
+     *                 });
+     *             }
+     *         }
+     *     }
+     * }
+     * ```
+     *
+     * ```html
+     * <InputArrayFilter
+     *          :base="baseArray"
+     *          v-model="resultArray"
+     *          :filter="inputFilter"/>
+     * ```
+     */
     export default {
         name: "InputArrayFilter",
         props: {
+
             title: {
                 type:       String,
                 required:   false,
                 default:    'Input Filter:'
             },
+            /**
+             * The base array on which the filters will be applied
+             */
             base: {
                 type:       Array,
                 required:   true,
             },
+            /**
+             * The array which results after the filter has been applied to the base
+             */
             value: {
                 type:       Array,
                 required:   true
             },
+            /**
+             * And object defining the filter, which is applied to the array
+             */
             filter: {
                 type:       Object,
                 required:   false,
@@ -47,25 +101,53 @@
         },
         data() {
             return {
+                // Contains the user input from the input field. Bound to the element via v-model
                 input: ''
             }
         },
         methods: {
+            /**
+             * Callback for the input even on the text input field. Will reapply the filter on the base array and
+             * emit the result as the parameter to an input event of this component
+             */
             onInput() {
                 let filtered = this.applyFilter();
                 this.emit(filtered);
             },
+            /**
+             * Applies the filter to the base array and returns the result.
+             *
+             * @returns {*}
+             */
             applyFilter() {
                 return this._applyFilter(this.base);
             },
+            /**
+             * Applies the filter on the given array.
+             *
+             * Defining helper methods with as many parameters as possible helps the testability.
+             *
+             * @param array
+             * @returns {*}
+             * @private
+             */
             _applyFilter(array) {
                 return this.filter['func'](array, this.input);
             },
+            /**
+             * Emits an "input" event with the given value.
+             *
+             * @param value
+             */
             emit(value) {
                 this.$emit('input', value);
             }
         },
         watch: {
+            /**
+             * Whenever the "base" prop is changed the filter will be applied to this new array.
+             * @param newValue
+             */
             base(newValue) {
                 let filtered = this._applyFilter(newValue);
                 this.emit(filtered);
