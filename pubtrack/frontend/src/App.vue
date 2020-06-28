4ec0956e33dcca6b61337abf1a632112c7a46397
the16thpythonist <jonseb1998@gmail.com>
2020-06-28 12:37:00 +0200

    Added a basic footer container to the main site layout

--- pubtrack/frontend/src/App.vue
+++ pubtrack/frontend/src/App.vue
@@ -4,6 +4,9 @@
     <div class="content">
       <router-view></router-view>
     </div>
+    <div class="footer">
+      Some content
+    </div>
   </div>
 </template>
 
@@ -19,14 +22,39 @@
 </script>
 
 <style>
+  html {
+    height: 100%;
+  }
+
   body {
     margin: 0;
     padding: 0;
+
+    min-height: 100%;
+    height: 100%
+  }
+
+  #app{
+    height: 100%;
+    min-height: 100%;
+
+    display: flex;
+    flex-direction: column;
   }
 
   .content {
     padding: 10px;
-    height: auto;
     margin: 30px 20% 0 20%;
+    flex-grow: 2;
+  }
+
+  .footer {
+    min-height: 50px;
+
+    margin-top: 50px;
+    padding: 20px;
+
+    background-color: #232323;
+    color: white;
   }
 </style>
